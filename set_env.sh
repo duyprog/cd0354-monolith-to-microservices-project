dcaf5ecc0306d64f779ff119e445376df303ce6d
duy-prog-2k <phamkhacduy.it@gmail.com>
2022-05-09 10:40:28 +0700

    fix

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=duyatt123
-export POSTGRES_HOST=udagram.c98ikvysumgl.ap-southeast-1.rds.amazonaws.com
-export POSTGRES_DB=udagram
-export AWS_BUCKET=duypk5-udagram-eks
-export AWS_REGION=ap-southeast-1
-export AWS_PROFILE=default
-export JWT_SECRET=testing
-export URL=http://localhost:8100
+POSTGRES_USERNAME=postgres
+POSTGRES_PASSWORD=duyatt123
+POSTGRES_HOST=udagram.c98ikvysumgl.ap-southeast-1.rds.amazonaws.com
+POSTGRES_DB=udagram
+AWS_BUCKET=duypk5-udagram-eks
+AWS_REGION=ap-southeast-1
+AWS_PROFILE=default
+JWT_SECRET=testing
+URL=http://localhost:8100
